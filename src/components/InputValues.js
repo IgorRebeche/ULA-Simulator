9635885fa6252f0bfc22694d02f1a666a1507827
Igor <igorrebeche@gmail.com>
2019-09-12 00:01:07 -0300

    a

--- src/components/InputValues.js
+++ src/components/InputValues.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Row, Col, Card, Input, Button } from "antd";
-import './InputValues.css'
+import "./InputValues.css";
 
 const InputValues = ({ children, title }) => {
   return (
     <Row type="flex" justify="center">
-      <Col span={12} >
-        <Card className = "inputInformation">
-            <Input placeholder="0000 0000 0000 0000"/>
-            <Button type="primary" size={18}>
-                Primary
-            </Button>
+      <Col>
+        <Card className="inputInformation">
+          <Row type="flex" justify="center" gutter={8}>
+            <Col>
+              <Input placeholder="0000 0000 0000" />
+            </Col>
+            <Col>
+              <Button type="primary" size={18}>
+                Calcular
+              </Button>
+            </Col>
+          </Row>
         </Card>
- 
       </Col>
     </Row>
   );
