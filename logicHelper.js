f1af3633ce5c5b1ab7171bb787b9eda2a548bbfe
Igor Rebeche | Stone <igor.rebeche@stone.com.br>
2019-09-20 13:25:57 -0300

    ax

--- logicHelper.js
+++ logicHelper.js
@@ -1,3 +1,6 @@
+//Ver Speedup funcionamento e no pipeline 
+//miguel.figuereido@gmail.com
+
 
 //Main
 var r1, r2, acc, flag, operando;
@@ -5,20 +8,21 @@ var r1, r2, acc, flag, operando;
 const Main = () => {
   //Initiating Registers
   console.log('Bem Vindo PC 16 BITS PC');
-  InitiateRegisters("0001 1011 1001 1111");
+  InitiateULA("0001 1001 1111");
   //sum(r1, r2);
-  operation(flag, r1, r2);
+  operation(operando, r1, r2);
   print('r1 ' + r1);
   print('r2 ' + r2);
-  print('flag ' + flag);    
+  print('operando ' + operando);
+  print('Teste', acc);    
 
 }
 
 // Methods Auxs 
-const InitiateRegisters = (binary) => {
+const InitiateULA = (binary) => {
    var codes = getBinary(binary);
    
-   flag = codes[0];
+   operando = codes[0];
    r1 = codes[1];
    r2 = codes[2];
 
@@ -27,31 +31,20 @@ const getBinary = (binary) => {
   var variables = [];
   return variables = binary.split(' ');
 }
-const operation = (operator) => {
-  switch(operator){
-    case op.SUM:
-      //sum()
-      console.log('sum');
-      break;
-    case op.SUB:
-      console.log('sub');
-      break;
-    case op.SHIFT_LEFT:
-      console.log('shift_left');
-      break;
-    case op.PRINT:
-      console.log('print');
-      break;
-      default:
-        break;
-  }
+const verify = (result) => {
+  if(result > 255)
+    flag = flagTypes.OVERFLOW;
+  if(result == 0)
+    flag = flagTypes.ZERO;
+  if(result < 0)
+    flag = flagTypes.NEGATIVE;
 }
 
 const sum = (var1, var2, accI) => {
   var x = parseInt(var1, 2);
   var y = parseInt(var2, 2);
   var sum = x + y;
-  acc = sum.toString(2);
+  return sum.toString(2);
 }
 
 const sub = (var1, var2, accI) => {
@@ -67,9 +60,39 @@ const print = ( information ) => {
 const op = {
   SUM: '0001',
   SUB: '0010',
+  MULT: '0011',
+  DIV: '0111',
   SHIFT_LEFT: '0100',
   PRINT: '1000'
 }
+const flagTypes = {
+  OVERFLOW: '0001',
+  NEGATIVE: '0010',
+  ZERO: '0100'
 
+}
 
+const operation = (operator, r1, r2) => {
+  switch(operator){
+    case op.SUM:
+      //sum()  
+      acc = sum(r1, r2);
+      console.log('sum ', acc);
+      break;
+    case op.SUB:
+      sub(r1, r2);
+      console.log('sub ', acc);
+      break;
+    case op.SHIFT_LEFT:
+
+      console.log('shift_left');
+      break;
+    case op.PRINT:
+      // Flag ( 0000 ) Valor ( 0000 0000 )
+      console.log('print');
+      break;
+      default:
+        break;
+  }
+}
 Main();
