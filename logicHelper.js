b5520351a731461b2ec072f51d017a8d90dd3688
Igor <igorrebeche@gmail.com>
2019-09-19 23:58:11 -0300

    awd

--- logicHelper.js
+++ logicHelper.js
@@ -1,23 +1,30 @@
 
 //Main
-var r1, r2, acc, flag, operando;
 
-const Main = () => {
+const ULA_App = (action) => {
   //Initiating Registers
   console.log('Bem Vindo PC 16 BITS PC');
-  InitiateRegisters("0001 1011 1001 1111");
+  var acc, flag;
+  var operando  = InitiateRegisters("0001 1011 1001 1111")[0];
+  var r1 = InitiateRegisters(action)[1];
+  var r2  = InitiateRegisters(action)[2];
   //sum(r1, r2);
-  operation(flag, r1, r2);
+  operation(operando, r1, r2, acc);
   print('r1 ' + r1);
   print('r2 ' + r2);
-  print('flag ' + flag);    
+  print('operando ' + operando);
+  
+  return ({
+    operationResult: '',
+    flag: ''
+  })
 
 }
 
 // Methods Auxs 
 const InitiateRegisters = (binary) => {
    var codes = getBinary(binary);
-   
+   return codes;
    flag = codes[0];
    r1 = codes[1];
    r2 = codes[2];
@@ -27,11 +34,11 @@ const getBinary = (binary) => {
   var variables = [];
   return variables = binary.split(' ');
 }
-const operation = (operator) => {
+const operation = (operator, r1, r2) => {
   switch(operator){
     case op.SUM:
-      //sum()
-      console.log('sum');
+      sum(r1, r2)
+      console.log('sum ' + acc);
       break;
     case op.SUB:
       console.log('sub');
@@ -47,9 +54,9 @@ const operation = (operator) => {
   }
 }
 
-const sum = (var1, var2, accI) => {
-  var x = parseInt(var1, 2);
-  var y = parseInt(var2, 2);
+const sum = () => {
+  var x = parseInt(r1, 2);
+  var y = parseInt(r2, 2);
   var sum = x + y;
   acc = sum.toString(2);
 }
@@ -72,4 +79,4 @@ const op = {
 }
 
 
-Main();
+ULA_App("0001 1011 1001 1111");
